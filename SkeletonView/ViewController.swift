0549603cd6b3a6279ecbd214593beaf828ad92ba
kuotinyen <kuotinyen@gmail.com>
2018-08-31 20:49:25 +0800

    init commit

---
+++ SkeletonView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkeletonView
+//
+//  Created by TING YEN KUO on 2018/8/31.
+//  Copyright © 2018年 TING YEN KUO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
